c852832167a749032a70aff2105c6842736e0717
brenno <brennoaraujoqueiroz@gmail.com>
2023-06-21 17:40:41 -0300

    chore: update boilerplate

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import json
 
-with open('boilerplate.json') as f:
+with open('boilerplate2.json') as f:
     data = json.load(f)
-    
-    
-for i in range(10):
-    with open(str(i)+'.json', 'w') as f:
+
+for i in range(888):
+    # Modifique a chave "name" para incluir o número da iteração atual
+    data["name"] = f"name example #{i+1}"
+
+    with open(str(i), 'w') as f:
         json.dump(data, f, indent=2)
-        print("New json file is created from boilerplate2.json file - number:"+str(i))
+
+    print("New json file is created from boilerplate2.json file - number:"+str(i))
